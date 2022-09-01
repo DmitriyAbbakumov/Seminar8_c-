fa669b5eece72d470cf7622de9d6e819d9ef52ab
Dmitriy <simpa-baki@mail.ru>
2022-09-01 21:13:28 +0300

    first commit

---
+++ Ex54/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] arr = new int[3, 5];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = new Random().Next(0, 10);
+        Console.Write(arr[i, j]);
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        for (int j1 = 0; j1 < arr.GetLength(1) - 1; j1++)
+        {
+            if (arr[i, j1 + 1] > arr[i, j1])
+            {
+                int max = arr[i, j1 + 1];
+                arr[i, j1 + 1] = arr[i, j1];
+                arr[i, j1] = max;
+            }
+        }
+    }
+}
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        Console.Write(arr[i, j]);
+    }
+    Console.WriteLine();
+}
---
+++ Ex56/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+Console.Write("Введите строки в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите столбцы в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int[m, n];
+int[] arr1 = new int[m];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = new Random().Next(0, 10);
+        Console.Write($"{arr[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        sum = sum + arr[i, j];
+    }
+    arr1[i] = sum;
+    Console.Write($"{arr1[i]} ");
+}
+Console.WriteLine();
+
+int min = arr1[0];
+for (int i = 0; i < arr1.Length; i++)
+{
+    if (min > arr1[i]) min = arr1[i];
+}
+Console.WriteLine(min);
+Console.WriteLine();
+
+for (int i = 0; i < arr1.Length; i++)
+    if (min == arr1[i]) Console.WriteLine($"Номер строки с наименьшей суммой элементов: {i + 1} строка");
