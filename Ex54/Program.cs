dba8545718438bc5318a96226431dd321cec106a
Dmitriy <simpa-baki@mail.ru>
2022-09-02 01:18:40 +0300

    test branch - ex54 sucsesfull

--- Ex54/Program.cs
+++ Ex54/Program.cs
@@ -8,38 +8,54 @@
 // 9 5 3 2
 // 8 4 4 2
 
-int[,] arr = new int[3, 5];
-for (int i = 0; i < arr.GetLength(0); i++)
+//int[,] arr = new int[3, 5];
+int[,] FillArray(int m, int n)  //Заполняет массив
 {
-    for (int j = 0; j < arr.GetLength(1); j++)
+    int[,] arr = new int[m, n];
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
-        arr[i, j] = new Random().Next(0, 10);
-        Console.Write(arr[i, j]);
-    }
-    Console.WriteLine();
-}
-Console.WriteLine();
-
-for (int i = 0; i < arr.GetLength(0); i++)
-{
-    for (int j = 0; j < arr.GetLength(1); j++)
-    {
-        for (int j1 = 0; j1 < arr.GetLength(1) - 1; j1++)
+        for (int j = 0; j < arr.GetLength(1); j++)
         {
-            if (arr[i, j1 + 1] > arr[i, j1])
+            arr[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return arr;
+}
+
+int[,] SortArray(int[,] arr)    //сортирует строки массива по убыванию
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int j1 = 0; j1 < arr.GetLength(1) - 1; j1++)
             {
-                int max = arr[i, j1 + 1];
-                arr[i, j1 + 1] = arr[i, j1];
-                arr[i, j1] = max;
+                if (arr[i, j1 + 1] > arr[i, j1])
+                {
+                    int max = arr[i, j1 + 1];
+                    arr[i, j1 + 1] = arr[i, j1];
+                    arr[i, j1] = max;
+                }
             }
         }
     }
+    return arr;
 }
-for (int i = 0; i < arr.GetLength(0); i++)
+
+void PrintArray(int[,] arr)         //Печать массива
 {
-    for (int j = 0; j < arr.GetLength(1); j++)
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
-        Console.Write(arr[i, j]);
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j]);
+        }
+        Console.WriteLine();
     }
-    Console.WriteLine();
 }
+
+int[,] array = FillArray(3, 5);
+PrintArray(array);
+Console.WriteLine();
+SortArray(array);
+PrintArray(array);
