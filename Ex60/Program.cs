966ef8ffca878e0049a14b0d7fd9259c43072107
Dmitriy <simpa-baki@mail.ru>
2022-09-02 20:09:07 +0300

    Ex60

--- Ex60/Program.cs
+++ Ex60/Program.cs
@@ -5,3 +5,43 @@
 // 34(1,0,0) 41(1,1,0)
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
+
+int[,,] Unique3DArray(int a, int b, int c, int startValue)
+{
+    int[,,] array = new int[a, b, c];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = startValue + i + j + k;
+                startValue = startValue + 2;
+            }
+
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,,] array)       //печатает матрицу
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,,] Unique3DArr = Unique3DArray(2, 2, 2, 10);
+PrintArray(Unique3DArr);
+
+
+
+
+
+
